2ec334f4742d8f559d11f4ce35edb522fcb8b22d
DWilliamZero <william.zero@gigkloud.com>
2020-04-14 13:56:52 -0400

    improve mobile responsiveness

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 
 function Login(props) {
   return (
-    <div className='login'>
+    <div className='login-form'>
       <form onSubmit={props.handleLogin}>
-        <input className='username'
-          type='text'
-          name='username'
-          placeholder='Username'
-          onChange={props.handleChange}
-          value={props.username} />
-        <input className='password'
-          type='password'
-          name='password'
-          placeholder='Password'
-          onChange={props.handleChange}
-          value={props.password} />
-        <button>Login</button>
+        <div className='login-form'>
+          <input className='username'
+            type='text'
+            name='username'
+            placeholder='Username'
+            onChange={props.handleChange}
+            value={props.username} />
+          <input className='password'
+            type='password'
+            name='password'
+            placeholder='Password'
+            onChange={props.handleChange}
+            value={props.password} />
+          <button type='submit'>Login</button>
+        </div>
       </form>
     </div>
   )
